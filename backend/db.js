d6e8b149caec14e6b52ad93bf76302b651353dcc
Yuvraj Pise <121165975+uraj7@users.noreply.github.com>
2024-04-20 23:53:17 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb+srv://piseyuvraj9:xv6dWTNgwGJd5m1C@cluster0.lfltnjh.mongodb.net/mefoodmern?retryWrites=true&w=majority';
+const mongoURI = 'mongodb+srv://username:@cluster0.lfltnjpasswordh.mongodb.net/mefoodmern?retryWrites=true&w=majority';
 
 const mongoDB = async () => {
     try {
